6faac39b2db22c5b8b6f84c2f76d3a1c01f6c7c6
Wim Haeck <whaeck@gmail.com>
2025-06-11 06:09:37 -0600

    Using shacl in dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,8 @@
 cmake_minimum_required( VERSION 3.27 )
-include( FetchContent )
+list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/.cmake)
+include( shacl_FetchContent )
 
-FetchContent_Declare( tools
+shacl_FetchContent_Declare( tools
     GIT_REPOSITORY  ../../njoy/tools
     GIT_TAG         7124bbfc746a9fa0e37a2c1e4a8eb019ddec9f02  # tag: v0.4.1
     )
@@ -11,25 +12,25 @@ FetchContent_Declare( tools
 #######################################################################
 
 if(ENDFtk.python)
-  FetchContent_Declare( pybind11
+  shacl_FetchContent_Declare( pybind11
       GIT_REPOSITORY  ../../pybind/pybind11
       GIT_TAG         5b0a6fc2017fcc176545afe3e09c9f9885283242 # tag: v2.10.4
       )
-  FetchContent_MakeAvailable(
+  shacl_FetchContent_MakeAvailable(
     pybind11
     )
 endif()
 
 if(ENDFtk.tests)
-  FetchContent_Declare( Catch2
+  shacl_FetchContent_Declare( Catch2
       GIT_REPOSITORY  ../../catchorg/Catch2
       GIT_TAG         3f0283de7a9c43200033da996ff9093be3ac84dc # tag: v3.3.2
       )
-  FetchContent_MakeAvailable(
-      Catch2
+  shacl_FetchContent_MakeAvailable(
+    Catch2
     )
 endif()
 
-FetchContent_MakeAvailable(
+shacl_FetchContent_MakeAvailable(
     tools
     )
