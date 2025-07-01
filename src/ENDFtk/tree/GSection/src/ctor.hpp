caec55de78ef9c507fb9ec29e920b9da7434e717
Wim Haeck <whaeck@gmail.com>
2025-07-01 12:43:21 -0600

    Adding the tree section for GENDF files

---
+++ src/ENDFtk/tree/GSection/src/ctor.hpp
@@ -0,0 +1,37 @@
+/**
+ *  @brief Constructor
+ *
+ *  This constructor is not to be called directly by a user.
+ *
+ *  @param[in] MAT      the MAT number of the section
+ *  @param[in] MF       the MF number of the section
+ *  @param[in] MT       the MT number of the section
+ *  @param[in] buffer   the buffer representing the section
+ */
+GSection( int MAT, int MF, int MT, std::string&& buffer ) :
+  mat_( MAT ), mf_( MF ), mt_( MT ), content_( std::move( buffer ) ) {}
+
+/**
+ *  @brief Constructor (from a buffer)
+ *
+ *  This constructor is not to be called directly by a user.
+ *
+ *  @param[in] head         the head record of the section
+ *  @param[in] begin        the beginning of the buffer
+ *  @param[in] position     the current position in the buffer
+ *  @param[in] end          the end of the buffer
+ *  @param[in] lineNumber   the current line number
+ */
+template< typename BufferIterator >
+GSection( const HEAD& head, BufferIterator begin,
+          BufferIterator& position, const BufferIterator& end,
+          long& lineNumber )
+  try: GSection( head.MAT(), head.MF(), head.MT(),
+                 std::string( begin, findEnd( position, end, lineNumber ) ) ) {}
+  catch( std::exception& e ) {
+
+    Log::info( "Trouble encountered while constructing a GENDF tree section" );
+    Log::info( "Current position: MAT{} MF{} MT{} at line {}",
+               head.MAT(), head.MF(), head.MT(), lineNumber );
+    throw e;
+  }
---
+++ src/ENDFtk/tree/GSection/src/findEnd.hpp
@@ -0,0 +1,15 @@
+template< typename BufferIterator >
+static BufferIterator findEnd
+( BufferIterator& position, const BufferIterator& end, long& lineNumber ) {
+
+  // skip to the first SEND record
+  while ( record::skip( position, end, lineNumber ).MT() != 0 ) {
+
+    if ( position >= end ) {
+
+      Log::error( "Encountered the end of stream before an END record was found" );
+      throw std::exception();
+    }
+  }
+  return position;
+}
