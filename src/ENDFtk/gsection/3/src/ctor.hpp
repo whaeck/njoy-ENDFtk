1f09759cf82d3e9aef84e6f7299ab09a40eed7a6
kitamuraa24 <kitamuraa24@tamu.edu>
2025-07-09 07:03:19 -0600

    Iniital 6g work. Currently compiles and passes basic verification of header. Additional testing of metadata required as well as a print method.

--- src/ENDFtk/gsection/3/src/ctor.hpp
+++ src/ENDFtk/gsection/3/src/ctor.hpp
@@ -1,73 +1,104 @@
+/**
+     *  @brief Constructor from parameters with ratio quantities
+     * 
+     *  @param[in] mt       the section number
+     *  @param[in] zaid     the ZAID identifier
+     *  @param[in] awr      the atomic weight ratio
+     *  @param[in] nl       the number of legendre moments
+     *  @param[in] nz       the number of dilution values
+     *  @param[in] lrflag   the break-up identifier flag
+     *  @param[in] ngn      the number of neutron energy groups
+     *  @param[in] temp     the temperature
+     *  @param[in] groups   the group index
+     *  @param[in] flux     the group-wise flux
+     *  @param[in] sigma    the group-wise cross section
+     *  @param[in] ratio    the group-wise ratios
+     */
     GType(int mt, int zaid, double awr, int nl, int nz, int lrflag,
         int ngn, double temp, std::vector<unsigned int > groups,
         std::vector < std::vector< std::vector<double> > > flux,
         std::vector < std::vector< std::vector<double> > > sigma,
         std::vector < std::vector< std::vector<double> > > ratio)
         :
-        Base(zaid, awr, mt), 
-        nl_(nl), 
-        nz_(nz), 
-        lrflag_(lrflag), 
-        ngn_(ngn),
-        temp_(temp),
-        groups_(groups),
-        flux_( std::move(flux) ), 
-        sigma_( std::move(sigma) ),
-        ratio_( std::move(ratio) ) {}
-
+        Base( zaid, awr, mt ), 
+        nl_( nl ), 
+        nz_( nz ), 
+        lrflag_( lrflag ), 
+        ngn_( ngn ),
+        temp_( temp ),
+        groups_( groups ),
+        flux_( std::move( flux ) ), 
+        sigma_( std::move( sigma ) ),
+        ratio_( std::move( ratio ) ) {}
+    
+/**
+     *  @brief Constructor from parameters without ratio quantities
+     * 
+     *  @param[in] mt       the section number
+     *  @param[in] zaid     the ZAID identifier
+     *  @param[in] awr      the atomic weight ratio
+     *  @param[in] nl       the number of legendre moments
+     *  @param[in] nz       the number of dilution values
+     *  @param[in] lrflag   the break-up identifier flag
+     *  @param[in] ngn      the number of neutron energy groups
+     *  @param[in] temp     the temperature
+     *  @param[in] groups   the group index
+     *  @param[in] flux     the group-wise flux
+     *  @param[in] sigma    the group-wise cross section
+     */
     GType(int mt, int zaid, double awr, int nl, int nz, int lrflag,
         int ngn, double temp, std::vector<unsigned int> groups,
         std::vector < std::vector< std::vector<double> > > flux,
         std::vector < std::vector< std::vector<double> > > sigma)
         :
-        Base(zaid, awr, mt), 
-        nl_(nl), 
-        nz_(nz), 
-        lrflag_(lrflag), 
-        ngn_(ngn),
-        temp_(temp),
-        groups_(groups),
-        flux_( std::move(flux) ), 
-        sigma_( std::move(sigma) ) {}
+        Base( zaid, awr, mt ), 
+        nl_( nl ), 
+        nz_( nz ), 
+        lrflag_( lrflag ), 
+        ngn_( ngn ),
+        temp_( temp ),
+        groups_( groups ),
+        flux_( std::move( flux ) ), 
+        sigma_( std::move( sigma ) ) {}
 
 private: 
 
     GType(int mt, int zaid, double awr, int nl, int nz, int lrflag, int ngn,
-        std::tuple<double,
-                   std::vector<unsigned int>,
-                   std::vector < std::vector< std::vector<double> > >,
-                   std::vector < std::vector< std::vector<double> > >>&& data)
+        std::tuple< double,
+                   std::vector< unsigned int >,
+                   std::vector < std::vector< std::vector< double > > >,
+                   std::vector < std::vector< std::vector< double > > >>&& data)
         :
         GType(mt, zaid, awr, nl, nz, lrflag, ngn,
-              std::move( std::get<0>(data) ),       // temp
-              std::move( std::get<1>(data) ),       // groups
-              std::move( std::get<2>(data) ),       // flux
-              std::move( std::get<3>(data) )) {}    // sigma
+              std::move( std::get<0>( data ) ),       // temp
+              std::move( std::get<1>( data ) ),       // groups
+              std::move( std::get<2>( data ) ),       // flux
+              std::move( std::get<3>( data ) )) {}    // sigma
 
     GType(int mt, int zaid, double awr, int nl, int nz, int lrflag, int ngn,
-        std::tuple<double,
-                   std::vector<unsigned int>,
-                   std::vector < std::vector< std::vector<double> > >,
-                   std::vector < std::vector< std::vector<double> > >,
-                   std::vector < std::vector< std::vector<double> > >>&& data)
+        std::tuple< double,
+                   std::vector< unsigned int >,
+                   std::vector < std::vector< std::vector< double > > >,
+                   std::vector < std::vector< std::vector< double > > >,
+                   std::vector < std::vector< std::vector< double > > >>&& data)
         :
-        GType(mt, zaid, awr, nl, nz, lrflag, ngn,
-              std::move( std::get<0>(data) ),       // temp
-              std::move( std::get<1>(data) ),       // groups
-              std::move( std::get<2>(data) ),       // flux 
-              std::move( std::get<3>(data) ),       // sigma
-              std::move( std::get<4>(data) )) {}    // ratio
+        GType( mt, zaid, awr, nl, nz, lrflag, ngn,
+              std::move( std::get<0>( data ) ),       // temp
+              std::move( std::get<1>( data ) ),       // groups
+              std::move( std::get<2>( data ) ),       // flux 
+              std::move( std::get<3>( data ) ),       // sigma
+              std::move( std::get<4>( data ) ) ) {}   // ratio
 
 /**
-     *  @brief Constructor from parameters
+     *  @brief Constructor from DataRecords
      * 
      *  @param[in] mt       the section number
      *  @param[in] zaid     the ZAID identifier
      *  @param[in] awr      the atomic weight ratio
-     *  @param[in] nl       the number of Legendre moments
+     *  @param[in] nl       the number of legendre moments
      *  @param[in] nz       the number of dilution values
      *  @param[in] lrflag   the break-up identifier flag
-     *  @param[in] ngn      the number of neutron energy bins
+     *  @param[in] ngn      the number of neutron energy groups
      *  @param[in] records  the data records 
      */
     GType(int mt, int zaid, double awr, int nl, int nz, int lrflag, int ngn,
---
+++ src/ENDFtk/gsection/6g.hpp
@@ -0,0 +1,149 @@
+#ifndef NJOY_ENDFTK_GSECTION_6
+#define NJOY_ENDFTK_GSECTION_6
+
+#include "ENDFtk/macros.hpp"
+#include "tools/std20/views.hpp"
+#include "ENDFtk/HeadRecord.hpp"
+#include "ENDFtk/DataRecord.hpp"
+#include "ENDFtk/ControlRecord.hpp"
+#include "tools/Log.hpp"
+#include "ENDFtk/section.hpp"
+#include <numeric>
+#include <iostream>
+
+namespace njoy {
+namespace ENDFtk {
+namespace section {
+
+    template < >
+    class ENDFTK_PYTHON_EXPORT GType< 6 > :
+        protected Base {
+
+        /* fields */
+        int nl_;
+        int nz_;
+        int lrflag_;
+        int ngn_;
+        double temp_;
+        std::vector< unsigned int > groups_;
+        std::vector< std::vector< std::vector< double > > > flux_;
+        std::vector< std::vector< std::vector< std::vector< double > > > > sigma_;
+        std::vector< std::vector< std::vector< double > > > probability_;
+        
+        /* auxiliary functions */
+        #include "ENDFtk/gsection/6/src/makeMatrices.hpp"
+        #include "ENDFtk/gsection/6/src/makeRecords.hpp"
+        #include "ENDFtk/gsection/6/src/readRecords.hpp"
+
+    public:
+
+        /* constructor */
+        #include "ENDFtk/gsection/6/src/ctor.hpp"
+        
+        /* methods */
+
+        /**
+         *  @brief Return the number of legendre moments.
+         */
+        int NL() const { return this->nl_; }
+
+        /**
+         *  @brief Return the number of legendre moments.
+         */
+        int numberMoments() const { return this->NL(); }
+
+        /**
+         *  @brief Return the number of dilution values.
+         */
+        int NZ() const { return this->nz_; }
+
+        /**
+         *  @brief Return the number of dilution values.
+         */        
+        int numberDilutions() const { return this->NZ(); }
+
+        /**
+         *  @brief Return the break up identifier flag.
+         */
+        int LRFLAG() const { return this->lrflag_; }
+
+        /**
+         *  @brief Return the break up identifier flag.
+         */
+        int breakUpID() const { return this->LRFLAG(); }
+
+        /**
+         *  @brief Return the number of neutron energy bins
+         */
+        int NGN() const { return this->ngn_; }
+
+        /**
+         *  @brief Return the number of neutron energy bins
+         */
+        int numberNeutronGroups() const { return this->NGN(); }
+
+        /**
+         *  @brief Return the temperature
+         */
+        double TEMP() const { return this->temp_; }
+        
+        /**
+         *  @brief Return the temperature
+         */
+        double temperature() const { return this->TEMP(); }
+
+        /**
+         *  @brief Return the group indices
+         */
+        auto groups() const { return this->groups_; }
+
+        /**
+         *  @brief Return the matrix for a given moment and dilution
+         * 
+         *  @param[in] moment   the legendre moment requested
+         *  @param[in] diltuion the dilution index requested
+         */
+        auto matrix(int moment, int dilution) const {
+            return this->sigma_[moment][dilution];
+        }
+        
+        /**
+         *  @brief Return the flux
+         * 
+         *  @param[in] moment   the legendre moment requested
+         *  @param[in] dilution the dilution index requested 
+         */
+        auto flux(int moment, int dilution) const {
+            return this->flux_[moment][dilution];
+        }
+
+        /**
+         *  @brief Return the probabilities
+         * 
+         *  @param[in] moment   the legendre moment requested
+         *  @param[in] dilution the dilution index requested 
+         */
+        auto probability(int moment, int dilution) const {
+            if ( this->probability_.size() != 0 ) {
+                return this->probability_[moment][dilution];
+            } else {
+                throw std::runtime_error("Requested probabilities when they are not present!");
+            }
+        }
+        #include "ENDFtk/gsection/6/src/print.hpp"
+
+        using Base::MT;
+        using Base::sectionNumber;
+        using Base::ZA;
+        using Base::targetIdentifier;
+        using Base::AWR; 
+        using Base::atomicWeightRatio;
+
+        };
+
+} // section
+} // ENDFtk
+} // njoy
+
+
+#endif // NJOY_ENDFTK_GSECTION_6
