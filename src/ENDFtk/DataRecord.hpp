197813e6c2c1a0d65bc0ecbce5aad8bb928ec9b4
Wim Haeck <whaeck@gmail.com>
2025-07-14 11:40:59 -0600

    Exposing the print function

--- src/ENDFtk/DataRecord.hpp
+++ src/ENDFtk/DataRecord.hpp
@@ -12,38 +12,38 @@ namespace ENDFtk {
     class DataRecord: protected ListRecord {
 
         public:
-        
+
         /**
          *  @brief constructor from parameters
-         * 
+         *
          *  @param[in]  temp    the temperature
          *  @param[in]  ng2     the number of secondary positions
          *  @param[in]  ig2lo   the index to lowest position
          *  @param[in]  nw      length of list
          *  @param[in]  ig      the group index
-         *  @param[in]  list    the data contained (i.e. fluxes, ratios, sigmas, matrix) 
+         *  @param[in]  list    the data contained (i.e. fluxes, ratios, sigmas, matrix)
          */
         DataRecord( double temp, int ng2, int ig2lo, int ig, std::vector<double>&& list):
         ListRecord(temp, 0, ng2, ig2lo, ig,
                    std::move(list)) {}
-        
+
         /**
          *  @brief constructor from a buffer
-         * 
+         *
          *  @tparam Iterator        buffer iterator
-         * 
+         *
          *  @param[in]  it          the current position in the buffer
          *  @param[in]  end         the end of the buffer
          *  @param[in]  lineNumber  the current line number
          *  @param[in]  MAT         the material number
          *  @param[in]  MF          the file number
-         *  @param[in]  MT          the section number      
+         *  @param[in]  MT          the section number
          */
         template<typename Iterator>
         DataRecord(Iterator& it, const Iterator& end, long& lineNumber,
                    int MAT, int MF, int MT):
                 ListRecord(it, end, lineNumber, MAT, MF, MT) {}
-        
+
 
         /* methods */
 
@@ -51,7 +51,7 @@ namespace ENDFtk {
          *  @brief Return the temperature
          */
         double TEMP() const {return this->C1();}
-        
+
         /**
          *  @brief Return the temperature
          */
@@ -98,8 +98,9 @@ namespace ENDFtk {
         int groupIndex() const {return this->IG();}
 
         using ListRecord::list;
+        using ListRecord::print;
 
-    };  
+    };
 
 
 } // ENDFtk
